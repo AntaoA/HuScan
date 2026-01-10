5e8da15ac040287f9bc34a6f1167927b83784014
AntaoA <amoryantao@net-c.com>
2026-01-10 22:54:25 +0100

    Ajout de la liste d'exclusion et des checks pour pattern4.kt

--- app/src/main/java/com/ascomany/camerajong/engine/patterns/pattern4.kt
+++ app/src/main/java/com/ascomany/camerajong/engine/patterns/pattern4.kt
@@ -1,40 +1,52 @@
 package com.ascomany.camerajong.engine.patterns
 
 import com.ascomany.camerajong.engine.Grouping
-import com.ascomany.camerajong.engine.Tile
-import com.ascomany.camerajong.engine.Wind
+import com.ascomany.camerajong.engine.isTerminalOrHonor
+import com.ascomany.camerajong.engine.WinType
 
 val pattern4 = listOf(
     Pattern(
-        id = 58,
-        name = "Last Tile",
-        description = "Winning on a tile that is the last of its kind. (It must be clear to all players based on the discards and exposures.)",
+        id = 55,
+        name = "Outside Hand",
+        description = "Hand includes terminals and honors in each set, including the pair.",
         points = 4,
         excludedPatternIds = emptyList(),
-        check = { hand -> 0}
-    ),
-    Pattern(
-        id = 57,
-        name = "Two Melded Kongs",
-        description = "A hand that includes two Melded Kongs. One Melded Kong and one Concealed Kong are 6 points.",
-        points = 4,
-        excludedPatternIds = emptyList(),
-        check = { hand -> 0}
+        check = { hand ->
+            val valid = hand.groupings.all { grouping ->
+                grouping.tiles.any { it.isTerminalOrHonor() }
+            }
+            if (valid) 1 else 0
+        }
     ),
     Pattern(
         id = 56,
         name = "Fully Concealed Hand",
-        description = "A hand that a player completes without any melds, and wins by Self-Draw.",
+        description = "A hand completed without any melds and won by self-draw.",
         points = 4,
-        excludedPatternIds = emptyList(),
-        check = { hand -> 0}
+        excludedPatternIds = listOf(80), // Self-Drawn
+        check = { hand ->
+            val isConcealed = hand.groupings.all { !it.isExposed }
+            if (isConcealed && hand.winType == WinType.SELF_DRAWN) 1 else 0
+        }
     ),
     Pattern(
-        id = 55,
-        name = "Outside Hand",
-        description = "A hand that includes terminals and honors in each set, including the pair.",
+        id = 57,
+        name = "Two Melded Kongs",
+        description = "Hand includes two melded kongs.",
         points = 4,
         excludedPatternIds = emptyList(),
-        check = { hand -> 0}
+        check = { hand ->
+            if (hand.groupings.count { it is Grouping.Kong && it.isExposed } == 2) 1 else 0
+        }
+    ),
+    Pattern(
+        id = 58,
+        name = "Last Tile",
+        description = "Winning on a tile that is the last of its kind.",
+        points = 4,
+        excludedPatternIds = emptyList(),
+        check = { hand ->
+            if (hand.isLastOfKind) 1 else 0
+        }
     )
 )
